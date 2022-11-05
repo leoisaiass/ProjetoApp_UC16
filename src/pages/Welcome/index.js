ebc7a9a36837a5883fa337b84782fb2066811c93
Leonardo Isaias <leoisaias135@gmail.com>
2022-11-05 16:35:37 -0300

    Criação Tela de Boas-Vindas e Login

---
+++ src/pages/Welcome/index.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+
+import * as Animatable from 'react-native-animatable'
+
+import { useNavigation } from '@react-navigation/native'
+
+export default function Welcome() {
+    const navigation = useNavigation();
+
+    return (
+        <View style={styles.container}>
+            
+            <View style={styles.containerLogo}>
+                <Animatable.Image
+                    animation="flipInY"
+                    source={require('../../../img/itemBrasao.png')}
+                    style={{width: '100%'}}
+                    resizeMode="contain"
+                />
+            </View>
+
+            <Animatable.View delay={600} animation="fadeInUp" style={styles.containerForm}>
+                <Text style={styles.title}>Leonardo</Text>
+                <Text style={styles.text}>Faça o login para começar</Text>
+
+                <TouchableOpacity 
+                    style={styles.button}
+                    onPress={ () => navigation.navigate('SingIn')}
+                    >
+                    <Text style={styles.buttonText}>Acessar</Text>
+                </TouchableOpacity>
+            </Animatable.View>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: 'gray'
+    },
+    containerLogo:{
+        flex:2,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }, 
+    containerForm:{
+        flex:1,
+        backgroundColor: '#FFFFFF',
+        borderRadius: 25,
+        borderTopRightRadius: 25,
+        paddingStart: '5%',
+        paddingEnd: '5%',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 20,
+        marginBottom: 12
+    },
+    text:{
+        color:"#a1a1a1"
+    },
+    button:{
+        position: "absolute", //botão flutuante
+        backgroundColor: "red",
+        borderRadius: 50,
+        paddingVertical: 8,
+        width: "60%",
+        alignSelf: "center",
+        bottom: "15%", 
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    buttonText:{
+        fontSize: 18,
+        color: "white",
+        fontWeight: "bold"
+    }
+});
